d74f41effbacf0d7a19a6ca736e4b6a1a8c4986c
Micaël Dupuis <micaeldupuis@live.fr>
2022-06-28 13:43:19 +0200

    Add .env for API url

--- src/services/authentication/Signup.js
+++ src/services/authentication/Signup.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function signup(form) {
   try {
-    const result = await axios.post("https://projet-technique-h3-2022.herokuapp.com/api/v1/user/register", {
+    const result = await axios.post(import.meta.env.VITE_URL_API + "/user/register", {
     	firstname: form.firstname,
 			lastname: form.lastname,
 			email: form.email,
