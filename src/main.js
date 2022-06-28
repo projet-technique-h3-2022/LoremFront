b62aa730e964f815c3beb83f83665b8a80ed4bd5
Micaël Dupuis <micaeldupuis@live.fr>
2022-06-28 16:25:58 +0200

    Get login user when re-open application

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router'
 
+import { useUserStore } from "@/services/UserStore";
+const { user } = useUserStore();
+
+if (localStorage.getItem("user")) user.value = JSON.parse(localStorage.getItem("user"));
+
 const app = createApp(App)
-
 app.use(router)
-
 app.mount('#app')
