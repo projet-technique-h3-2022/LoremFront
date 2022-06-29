0acbf215110c771880282dc7ffca84bc6e4f7a34
Frat Ulker <frat@cleanmob.eu>
2022-06-29 14:31:26 +0200

    add: edit article feature

--- src/services/article/article.js
+++ src/services/article/article.js
@@ -20,3 +20,24 @@ export async function getArticlesByAuthor() {
     return err.response.data.error;
   }
 }
+
+export async function getArticleById(articleId) {
+    try {
+        const result = await axios.get(import.meta.env.VITE_URL_API + "/article/" + articleId);
+        return result.data
+    } catch (err) {
+        return err.response.data.error;
+    }
+}
+
+export async function putArticle(article) {
+    try {
+        const result = await axios.put(import.meta.env.VITE_URL_API + "/article/" + article.id, {
+            title: article.title,
+            image: article.image,
+            content: article.content,
+        });
+    } catch (err) {
+        return err.response.data.error;
+    }
+}
