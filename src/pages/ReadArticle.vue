72e552bfb51cdbfead9e19810fec5a8eb8c9acd7
Frat Ulker <frat@cleanmob.eu>
2022-06-30 11:32:43 +0200

    add: link for ReadArticle component

--- src/pages/ReadArticle.vue
+++ src/pages/ReadArticle.vue
@@ -1,32 +1,34 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
+import { getArticleById } from '../services/article/article';
+
+const route = useRoute()
+const articleId = ref("")
+const article = ref({})
+articleId.value = route.params.id
+
+onMounted(async () => {
+    article.value = await getArticleById(articleId.value)
+})
+
+</script>
+
 <template>
     <div class="ReadArticle">
         <div class="articleImage">
             <img src="https://picsum.photos/1200/200" class="img-fluid" alt="..." />
-            <h1 class="articleTitle">Article Title</h1>
+            <h1 class="articleTitle">{{article.title}}</h1>
         </div>
         <div class="articleContent">
             <div class="row">
-                <div class="col-md-2 leftContent">
-                    <div>Publié le 1900</div>
-                    <div>Ecrit par John Doe</div>
-                    <div>Lien vers le groupe</div>
+                <div class="col-md-3 leftContent">
+                    <div>Publié le {{(new Date(article?.published_at)).toLocaleDateString()}}</div>
+                    <div>Ecrit par {{article?.id_author?.firstname + " " + article?.id_author?.lastname}}</div>
+                    <div>Paru dans le groupe {{article?.id_group?.title}}</div>
                 </div>
-                <div class="col-md-8 mainContent">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea at, molestias explicabo aperiam,
-                    eveniet iure dignissimos ut omnis in enim repellendus porro voluptatem ex inventore recusandae!
-                    Nesciunt vel quos libero.
-                    Laudantium reprehenderit amet porro, commodi recusandae aliquid repellat, necessitatibus ad
-                    dolore saepe perferendis nam nostrum, officia ratione? Aspernatur distinctio ab obcaecati
-                    molestiae saepe numquam alias ut eveniet, doloremque facilis enim.
-                    Odit possimus placeat magni, dolores inventore ipsum illo, pariatur tempore debitis, tempora at
-                    libero obcaecati laborum. Amet soluta labore odit iure suscipit, consequatur saepe eos ducimus
-                    dolor facere vel aut.
-                    Eius illum suscipit dolor veniam nemo minima commodi, ipsum, optio sapiente quo, vel iste nam
-                    laboriosam. Dolore consectetur facilis dolor ullam autem, eum id exercitationem obcaecati saepe
-                    deleniti possimus a?
-                    Consequatur excepturi eius at. Unde voluptatem commodi deleniti ipsum autem laudantium, nostrum
-                    error doloribus eaque, accusantium necessitatibus ex. Facere nesciunt numquam eum veritatis
-                    repellendus. Neque illum suscipit quae eos repellat.
+                <div class="col-md-6 mainContent">
+                    {{article.content}}
                 </div>
             </div>
         </div>
